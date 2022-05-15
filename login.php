092d0b982bc520361c0df75c8bcf16c25c6eaa4a
mervesenturkk <104272489+mervesenturkk@users.noreply.github.com>
2022-05-15 21:26:57 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="login.css">
+    <script src="bootstrap.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg nav-bg">
+        <div class="container">
+          <a class="navbar-brand text-white" href="#">MERVE ŞENTÜRK</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link active" aria-current="page" href="anasayfa.html">Hakkımda</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="ozgecmisim.html">Özgeçmişim</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="sehrim.html">Şehrim</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="mirasimiz.html">Mirasımız</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="ilgiAlan.html">İlgi Alanlarım</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="login.html">Login</a>
+                <span class="ayrac">|</span>
+              </li>
+              <li class="nav-item d-flex align-items-center">
+                <a class="nav-link" href="iletisim.html">İletişim</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h3 class="text-center">GİRİŞ YAPILDI</h3>
+    </div>
+    <div class="container mt-3 text-center">
+    <?php
+    echo "Hoşgeldiniz b211210087"."<br>";
+    ?>
+    
+    </div>
+    
+</body>
+</html>
